b78263d38d0f34a7f0fc58dc21a0a58288f250c6
Guiselle Tatiana Zambrano Penagos <guiselle2097@gmail.com>
2019-10-23 14:11:29 -0500

    I created a test grammar and modified the function to get the first set

--- prediction_set_generator.py
+++ prediction_set_generator.py
@@ -29,34 +29,44 @@ def get_grammar():
             grammar_dic[temp_key].append(temp_rules)
     return grammar_dic
 
-def set_of_first(grammar_dic, first_dict, key):
-    if key in first_dict:
-        return
-    else:
-        rule_list = grammar_dic[key]
+def get_set_of_first(key, grammar_dic, first_dic):
+    if key not in first_dic:
         first_set = set()
-        for rules_tuple in rule_list:
+        for rules_tuple in grammar_dic[key]:
             for rule in rules_tuple:
                 split_rule = rule.split(' ')
                 first = split_rule[0]
                 if re.match("(^\'.*\'.*)", first) is None:
-                    temp_set = set_of_first(grammar_dic, first_dict, first)
-                    temp_set.discard(' ')
-                    first_set = first_set | temp_set
+                    aux = get_set_of_first(first, grammar_dic, first_dic)
+                    set_1 = aux[1]
+                    if "''" in set_1:
+                        first = split_rule[1]
+                        aux = get_set_of_first(first, grammar_dic, first_dic)
+                        set_2 = aux[1]
+                        set_1 = set_1 | set_2
+                        set_1.discard(' ')
+                    first_set = first_set | set_1
                 else:
                     first_set.add(first)
-        first_dict[key] = first_set
-        print(first_dict)
-        return first_set
+        first_dic[key] = first_set
+        return (first_dic, first_set)
+
+
+def get_first_dic(grammar_dic):
+    first_dic = dict()
+    for key in grammar_dic:
+        aux_dic = get_set_of_first(key, grammar_dic, first_dic)
+        if aux_dic is None:
+            break
+        first_dic = aux_dic[0]
+    return first_dic
 
 
 def main():
     cls()
     grammar_dic = get_grammar()
-
-    first_dict = dict()
-    for key in grammar_dic:
-        set_of_first(grammar_dic, first_dict, key)
+    first_dic = get_first_dic(grammar_dic)
+    print (first_dic)
     
 
 if __name__ == '__main__':
