f3bfb1c0b6bef1a7c73e908930ad72a5a982aff3
Guiselle Tatiana Zambrano Penagos <guiselle2097@gmail.com>
2019-10-27 00:32:44 -0500

    I added function in the 'ImportantSets' class
    I completed the function of the first set and declared the auxiliary
    functions to obtain the seconds set and the prediction set.
    I also added the path to read the grammar file called 'SR_grammar.txt'.

--- practice_2.py
+++ practice_2.py
@@ -1,4 +1,9 @@
+import re
 from os import system, name
+from pathlib import Path
+
+LOCAL_FOLDER = Path(__file__).parent
+GRAMAR_FL = LOCAL_FOLDER / 'data/SR_grammar.txt'
 
 
 def cls():
@@ -37,38 +42,83 @@ class Rule():
 
 class ImportantSets():
     def __init__(self, grammar):
-        self.__grammar = self.__get_grammar()
-        self.__set_of_the_firsts = set()
-        self.__set_of_the_seconds = set()
-        self.__prediction_set = set()
+        self.__grammar = self.__get_grammar(grammar)
+        self.__dict_of_the_first = dict()
+        self.__seconds_dict = dict()
+        self.__prediction_dict = dict()
+        self.__get_dict_of_the_first()
+        self.__get_dict_of_the_seconds()
+        self.__get_prediction_dict()
 
-    def __get_grammar(grammar):
+    def __get_grammar(self, grammar):
         grammar_dict = dict()
         for line in grammar:
             line = line.strip()
             line = line.split("\t->\t")
             key = line[0]
-            rules = tuple(line[1].split(" | "))
+            rules = line[1].split(" | ")
+
+            rules_list = list()
+            for rule in rules:
+                rule = Rule(rule)
+                rules_list.append(rule)
+            rules_tupl = tuple(rules_list)
 
             if grammar_dict.get(key) is None:
                 grammar_dict[key] = list()
 
-            grammar_dict[key].append(rules)
+            grammar_dict[key].append(rules_tupl)
         return grammar_dict
 
-    def __get_set_of_the_first():
+    def __get_set_of_the_first(self, key):
+        if key not in self.__dict_of_the_first:
+            set_of_the_first = set()
+            for rules_tupl in self.__grammar[key]:
+                for rule in rules_tupl:
+                    first = rule.get_first()
+                    if re.match("(^\'.*\'.*)", first) is None:
+                        aux_set_1 = self.__get_set_of_the_first(first)
+                        if "''" in aux_set_1:
+                            first = rule.get_symbol(1)
+                            aux_set_2 = self.__get_set_of_the_first(first)
+                            aux_set_1 = aux_set_1 | aux_set_2
+                            aux_set_1.discard("''")
+                        set_of_the_first = set_of_the_first | aux_set_1
+                    else:
+                        set_of_the_first.add(first)
+            self.__dict_of_the_first[key] = set_of_the_first
+            return set_of_the_first
+
+    def __get_dict_of_the_first(self):
+        for key in self.__grammar:
+            if key not in self.__dict_of_the_first:
+                self.__get_set_of_the_first(key)
+
+    def __get_set_of_the_secods(self, key):
+        pass
+
+    def __get_dict_of_the_seconds(self):
+        pass
+
+    def __get_prediction_dict(self):
+        pass
+
+    def get_dict_of_the_first(self):
+        return self.__dict_of_the_first
+
+    def get_set_of_the_secods(self):
+        pass
+
+    def get_prediction_dict(self):
         pass
 
 
 def main():
     cls()
-    test = Rule("'a' B 'c'")
-    q = test.get_symbol(1)
-    print(q)
-    q = test.get_first()
-    print(q)
-    q = test.get_second('B')
-    print(q)
+    with open(GRAMAR_FL, 'r') as fl:
+        grammar = fl.readlines()
+        sets = ImportantSets(grammar)
+        print(sets.get_dict_of_the_first())
 
 
 if __name__ == '__main__':
