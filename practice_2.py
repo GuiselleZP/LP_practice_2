221d6b4241abd1c6d3102702d3e22a3fbede466b
Guiselle Tatiana Zambrano Penagos <guiselle2097@gmail.com>
2019-10-28 21:39:28 -0500

    I created the functions of the different sets
    These functions have failures when there is recursion either on the
    right or the left.

--- practice_2.py
+++ practice_2.py
@@ -14,15 +14,16 @@ def cls():
     else:
         system('clear')
 
+
 class ImportantSets():
     def __init__(self, grammar):
         self.__grammar = self.__get_grammar(grammar)
         self.__dict_of_the_first = dict()
         self.__dict_of_the_following = dict()
-        self.__prediction_dict = dict()
+        self.__prediction_sets_list = list()
         self.__get_dict_of_the_first()
         self.__get_dict_of_the_following()
-        self.__get_prediction_dict()
+        self.__get_prediction_sets_list()
 
     def __get_grammar(self, grammar):
         grammar_dict = dict()
@@ -30,12 +31,12 @@ class ImportantSets():
             line = line.strip()
             line = line.split("\t->\t")
             key = line[0]
-            rules = tuple(line[1].split(' | '))
-            
+            rule = line[1]
+
             if grammar_dict.get(key) is None:
                 grammar_dict[key] = list()
 
-            grammar_dict[key].append(rules)
+            grammar_dict[key].append(rule)
         return grammar_dict
 
     def __is_a_non_terminal(self, symbol):
@@ -43,17 +44,15 @@ class ImportantSets():
             return True
         else:
             return False
-        
+
     def __call_first_set_non_terminal(self, symbol):
         if symbol not in self.__dict_of_the_first:
             set_first = set()
-            for rules_tupl in self.__grammar[symbol]:
-                for rule in rules_tupl:
-                    aux = self.__get_set_of_the_first(rule)
-                    set_first = aux | set_first
+            for rule in self.__grammar[symbol]:
+                aux = self.__get_set_of_the_first(rule)
+                set_first = aux | set_first
             self.__dict_of_the_first[symbol] = set_first
         return self.__dict_of_the_first[symbol]
-
 
     def __get_set_of_the_first(self, rule):
         empty_symbol = False
@@ -72,7 +71,7 @@ class ImportantSets():
                 aux_set.discard("''")
         else:
             if "''" == symbol:
-                aux_set.add("''")              
+                aux_set.add("''")
             else:
                 aux_set.add(symbol)
         set_first = set_first | aux_set
@@ -82,21 +81,77 @@ class ImportantSets():
             aux_set = self.__get_set_of_the_first(leftover)
             set_first = set_first | aux_set
         return set_first
-                   
-                    
+
     def __get_dict_of_the_first(self):
         for key in self.__grammar:
             if key not in self.__dict_of_the_first:
-                  self.__call_first_set_non_terminal(key)
+                self.__call_first_set_non_terminal(key)
 
-    def __get_set_of_the_following(self, symbol):
-        pass
-       
+    def __search_symbol(self, rule, symbol):
+        result = None
+        for index in range(len(rule)):
+            if rule[index] == symbol:
+                index += 1
+                result = rule[index:]
+                result = result.strip()
+                break
+        return result
+
+    def __call_following_set_non_terminal(self, symbol):
+        if symbol not in self.__dict_of_the_following:
+            set_following = set()
+            aux_set = set()
+            if symbol == 'A':
+                set_following.add("'$'")
+            for key in self.__grammar:
+                for rule in self.__grammar[key]:
+                    aux_set = self.__get_set_of_the_following(key, rule,
+                                                              symbol)
+                    set_following |= aux_set
+                self.__dict_of_the_following[symbol] = set_following
+        return self.__dict_of_the_following[symbol]
+
+    def __get_set_of_the_following(self, key, rule, symbol):
+        leftover = self.__search_symbol(rule, symbol)
+        set_following = set()
+        if leftover is not None:
+            empty_symbol = False
+            aux_set = set()
+            if len(leftover) > 0:
+                aux_set = self.__get_set_of_the_first(leftover)
+                if "''" in aux_set:
+                    empty_symbol = True
+                    temp_set = aux_set
+                    aux_set = set()
+                    aux_set |= temp_set
+                    aux_set.discard("''")
+                set_following |= aux_set
+            if len(leftover) == 0 or empty_symbol:
+                aux_set = self.__call_following_set_non_terminal(key)
+                set_following |= aux_set
+        return set_following
+
     def __get_dict_of_the_following(self):
-        pass
+        for key in self.__grammar:
+            if key not in self.__dict_of_the_following:
+                self.__call_following_set_non_terminal(key)
 
-    def __get_prediction_dict(self):
-        pass
+    def __get_prediction_set(self, key, rule):
+        prediction_set = self.__get_set_of_the_first(rule)
+        if "''" in prediction_set:
+            temp_set = prediction_set
+            prediction_set = set()
+            prediction_set |= temp_set
+            prediction_set.discard("''")
+            prediction_set |= self.__dict_of_the_following[key]
+        return prediction_set
+
+    def __get_prediction_sets_list(self):
+        for key in self.__grammar:
+            for rule in self.__grammar[key]:
+                prediction_set = self.__get_prediction_set(key, rule)
+                self.__prediction_sets_list.append(prediction_set)
+
     def get_dict_of_grammar(self):
         return self.__grammar
 
@@ -104,10 +159,10 @@ class ImportantSets():
         return self.__dict_of_the_first
 
     def get_dict_of_the_following(self):
-        pass
+        return self.__dict_of_the_following
 
-    def get_prediction_dict(self):
-        pass
+    def get_prediction_sets_list(self):
+        return self.__prediction_sets_list
 
 
 def main():
@@ -117,6 +172,9 @@ def main():
         sets = ImportantSets(grammar)
         print(sets.get_dict_of_grammar())
         print(sets.get_dict_of_the_first())
+        print(sets.get_dict_of_the_following())
+        print(sets.get_prediction_sets_list())
+
 
 if __name__ == '__main__':
     main()
