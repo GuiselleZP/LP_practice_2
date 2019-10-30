408f355a0d50199e897674eb0c8036a3e4040ac2
Guiselle Tatiana Zambrano Penagos <guiselle2097@gmail.com>
2019-10-30 04:19:39 -0500

    I changed the name of the file
    Because I will work with another module.

--- important_sets.py
+++ important_sets.py
@@ -15,7 +15,7 @@ def cls():
         system('clear')
 
 
-class ImportantSets():
+class Sets():
     def __init__(self, grammar):
         self.__grammar = self.__get_grammar(grammar)
         self.__dict_of_the_first = dict()
@@ -55,7 +55,6 @@ class ImportantSets():
         if symbol not in self.__dict_of_the_first:
             set_first = set()
             for rule in self.__grammar[symbol]:
-                print('F:{}: -> :{}:'.format(symbol, rule))
                 if symbol == rule[0]:
                     if "''" in set_first:
                         rule = rule[1:]
@@ -63,11 +62,7 @@ class ImportantSets():
                     else:
                         continue
                 aux = self.__get_set_of_the_first(symbol, rule)
-                print('conjunto en call de', rule)
-                print(aux)
                 set_first = aux | set_first
-            print('PRIMEROS DE', symbol)
-            print(set_first)
             self.__dict_of_the_first[symbol] = set_first
         return self.__dict_of_the_first[symbol]
 
@@ -79,15 +74,10 @@ class ImportantSets():
         symbol = aux[0]
 
         if key == symbol:
-            print('key:{}:symbol:{}:'.format(key, symbol))
             return set_first
 
         if self.__is_a_non_terminal(symbol):
-            print('call secundaria para buscar primeros de', symbol)
             aux_set = self.__call_first_set_non_terminal(symbol)
-            print('conjunto de:', symbol)
-            print(aux_set)
-            print('len:', len(rule))
             if "''" in aux_set and len(rule) > 1:
                 epsilon = True
                 temp_set = aux_set
@@ -97,28 +87,20 @@ class ImportantSets():
         else:
             if "''" == symbol:
                 aux_set.add("''")
-                print('epsilon')
             else:
-                print('terminal')
                 aux_set.add(symbol)
         set_first = set_first | aux_set
 
         if len(aux) == 2 and epsilon:
-            print('tiene epsilon')
             leftover = aux[1]
-            print('recursion desde first busca primeros de:{}:'.format(leftover))
             aux_set = self.__get_set_of_the_first(key, leftover)
             set_first = set_first | aux_set
-        print('conjunto primeros de', rule)
-        print(set_first)
         return set_first
 
     def __get_dict_of_the_first(self):
         for key in self.__grammar:
             if key not in self.__dict_of_the_first:
                 self.__call_first_set_non_terminal(key)
-        print('DICCIONARIO PRIMEROS')
-        print(self.__dict_of_the_first)
 
     def __search_symbol(self, rule, symbol):
         result = None
@@ -140,29 +122,21 @@ class ImportantSets():
                 set_following.add("'$'")
             for key in self.__grammar:
                 for rule in self.__grammar[key]:
-                    print('S:{}: -> :{}:sym:{}:'.format(key, rule, symbol))
 
                     aux_set = self.__get_set_of_the_following(key, rule,
                                                               symbol)
                     set_following |= aux_set
                 self.__dict_of_the_following[symbol] |= set_following
-            print('SIGUIENTES DE', symbol)
-            print(set_following)
         return self.__dict_of_the_following[symbol]
 
     def __get_set_of_the_following(self, key, rule, symbol):
         leftover = self.__search_symbol(rule, symbol)
         set_following = set()
         if leftover is not None:
-            print('set_foll key:{}:rule:{}:symbol:{}'.format(key, rule, symbol))
             epsilon = False
             aux_set = set()
             if len(leftover) > 0:
-                print('hay sig:{}'.format(leftover))
-                print('recursion desde sigiente')
                 aux_set = self.__get_set_of_the_first('', leftover)
-                print('conjunto de lef:{}:'.format(leftover))
-                print(aux_set)
                 if "''" in aux_set:
                     epsilon = True
                     temp_set = aux_set
@@ -170,52 +144,35 @@ class ImportantSets():
                     aux_set |= temp_set
                     aux_set.discard("''")
                 set_following |= aux_set
-                   
+
             if len(leftover) == 0 or epsilon:
-                print('lef:{}:epsilon:{}'.format(leftover, epsilon))
-                print('llamada call desde sig')
                 if key in self.__dict_of_the_following:
                     aux_set = self.__dict_of_the_following[key]
                     set_following |= aux_set
                 aux_set = self.__call_following_set_non_terminal(key)
-                print('conjunto de', key)
-                print(aux_set)
                 set_following |= aux_set
-        print('conjunto sig')
-        print(set_following)
         return set_following
 
     def __get_dict_of_the_following(self):
         for key in self.__grammar:
             if key not in self.__dict_of_the_following:
                 self.__call_following_set_non_terminal(key)
-        print('DICCIONARIO SIGUIENTES')
-        print(self.__dict_of_the_following)
 
     def __get_prediction_set(self, key, rule):
-        print('llamando primeros de:{}:'.format(rule))
         prediction_set = self.__get_set_of_the_first(key, rule)
-        print('primeros obtenidos')
-        print(prediction_set)
         if "''" in prediction_set:
             temp_set = prediction_set
             prediction_set = set()
             prediction_set |= temp_set
             prediction_set.discard("''")
-            print('llamado a siguientes de', key)
             prediction_set |= self.__dict_of_the_following[key]
-        print('conjunto pred')
-        print(prediction_set)
         return prediction_set
 
     def __get_prediction_sets_list(self):
         for key in self.__grammar:
             for rule in self.__grammar[key]:
-                print('P:{}: -> :{}:'.format(key, rule))
                 prediction_set = self.__get_prediction_set(key, rule)
                 self.__prediction_sets_list.append(prediction_set)
-        print('LISTA CONJUNTOS PREDICCION')
-        print(self.__prediction_sets_list)
 
     def get_dict_of_grammar(self):
         return self.__grammar
@@ -234,7 +191,7 @@ def main():
     # cls()
     with open(GRAMAR_FL, 'r') as fl:
         grammar = fl.readlines()
-        sets = ImportantSets(grammar)
+        sets = Sets(grammar)
         print(sets.get_dict_of_grammar())
         print(sets.get_dict_of_the_first())
         print(sets.get_dict_of_the_following())
